5f955930ad8f4a3bb2536338abd50813e7f44235
Ankit pal <ankit1710pal@gmail.com>
2026-05-21 07:44:48 +0000

    conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,24 @@
+let  score=33
+//console.log(typeof score);
+//console.log(typeof(score)); 
+
+let valueINnumber= Number(score)
+//console.log(typeof valueINnumber);
+//console.log(valueINnumber);
+
+
+//"33"=>33
+//"33abc"=.NaN
+//true=>1; false=>0
+
+let isLoggedIn = "Ankit"
+let booleanIsLoggedIN = Boolean(isLoggedIn)
+//console.log(booleanIsLoggedIN);
+
+// 1=>true; 0=> false
+// ""=> false
+// "hitesh"=> true
+let someNumber=33
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
