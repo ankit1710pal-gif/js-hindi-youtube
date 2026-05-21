2bcc460281341bf07c34118115d81c2bc58937e9
Ankit pal <ankit1710pal@gmail.com>
2026-05-21 07:16:25 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,18 @@
+const accountId=144553
+let accountEmail="Ankit@google.com"
+var accountPassword="12345"
+accountCity="Delhi"
+//accountId=2
+
+console.log(accountId);
+let accountState;
+accountEmail="hc@hc.com"
+accountPassword="67890"
+accountCity="bengalure"
+
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+/*
+prefer not to use var
+because of issue in the block scope and functional scope
+*/
---
+++ 01_basics/02_dataType.js
@@ -0,0 +1,20 @@
+"use strict"; //treat all js code as newer version
+//alert( 3 + 3) // we are using nodejs, not browser
+
+console.log(3 + 3); 
+console.log("Ankit")
+//code readbility should be high
+
+//number=> 2 to power 53
+//bigint
+//string =>""
+//boolean=> true/false
+//null is standlone value and null type is object
+//undefined=>
+//symbol =. when we talk about unique
+
+//object
+
+console,log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof null);
