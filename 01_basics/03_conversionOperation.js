cdf00c582d954a7ebb183ee80ceb4609831714d1
Ankit pal <ankit1710pal@gmail.com>
2026-05-23 05:23:44 +0000

    string ke bareme

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -20,5 +20,20 @@ let booleanIsLoggedIN = Boolean(isLoggedIn)
 // "hitesh"=> true
 let someNumber=33
 let stringNumber = String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//*********************** operations*********** */
+
+
+let value = 3;
+let negValue = -value
+//console.log(negValue);
+
+//console.log(+true);
+//console.log(+""); 
+
+let num1, num2, num3=2+2
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
