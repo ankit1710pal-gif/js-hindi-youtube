27ff81e71e15d30838db624ddf81144abbe2b6e6
Ankit pal <ankit1710pal@gmail.com>
2026-05-24 10:49:41 +0000

    function 1

---
+++ 03_basics/01_functions.js
@@ -0,0 +1,20 @@
+function sayMyName() {
+    console.log("H");
+    console.log("I");
+    console.log("T");
+    console.log("E");
+    console.log("S");
+    console.log("H");
+}
+
+sayMyName();
+
+function addTwoNumbers(number1, number2) {
+  //  console.log(number1 + number2);
+}
+
+addTwoNumbers(3, null);
+
+const result = addTwoNumbers(3, 5);
+
+console.log("Result:", result);
